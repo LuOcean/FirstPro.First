fc847b1789d2169c2b24f7902a56d4c551c913c9
luhaiyang <luhaiyang@yiguo.com>
2017-05-15 16:43:06 +0800

    [mod]modify class name UserInfo

---
+++ FirstPro/FirstPro.First/UserInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPro.First
+{
+    public class UserInfo
+    {
+        private string id;
+        private string name;
+        private int age;
+
+        public UserInfo()
+        {
+
+        }
+        public string Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+    }
+}
